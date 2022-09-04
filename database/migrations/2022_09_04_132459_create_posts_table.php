c4dff384420052d7482bdd5c6e576ddca7ece766
Roman Hossain Shaon <roman.shaon@gmail.com>
2022-09-04 19:41:52 +0600

    post table applyed softdelete

--- database/migrations/2022_09_04_132459_create_posts_table.php
+++ database/migrations/2022_09_04_132459_create_posts_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 

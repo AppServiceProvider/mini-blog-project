7fc56093d893f43c3577fc0c86aaed6a39dea37d
Roman Hossain Shaon <roman.shaon@gmail.com>
2022-09-04 20:02:50 +0600

    seeder & factories usage Post table

---
+++ database/seeders/PostSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Post;
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Post::factory()
+        ->count(50)
+        ->create([
+            'user_id' => '1',
+        ]);
+    }
+}

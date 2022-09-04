746eba44edc535a7ae26a6c5a4542229a8befeb6
Roman Hossain Shaon <roman.shaon@gmail.com>
2022-09-04 19:38:41 +0600

    post table join user table foreign key

--- database/migrations/2022_09_04_132459_create_posts_table.php
+++ database/migrations/2022_09_04_132459_create_posts_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('body');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
